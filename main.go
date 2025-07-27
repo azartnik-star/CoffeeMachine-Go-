07ad14d53ae0233d6c586ca4beb1f876a224d006
Ilya <ia.azarovilya2001@mail.ru>
2025-07-27 15:12:54 +1000

    feat: Первое знакомство с кодом. разговор кода с покупателем

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import f "fmt"
+
+func main() {
+	f.Println("Starting to make a coffee")
+	f.Println("Grinding coffee beans")
+	f.Println("Boiling water")
+	f.Println("Mixing boiled water with crushed coffee beans")
+	f.Println("Pouring coffee into the cup")
+	f.Println("Pouring some milk into the cup")
+	f.Println("Coffee is ready!")
+}
