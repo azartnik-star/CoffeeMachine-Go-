9fa1d834dae5e9283d45325174b114615359987f
Ilya <ia.azarovilya2001@mail.ru>
2025-09-05 05:42:39 +1000

    feat: продолжил pet-project, написал программу по покупке напитков. дальше снятие денег и пополнение содержимого в кофемашине

--- main.go
+++ main.go
@@ -2,55 +2,51 @@ package main
 
 import f "fmt"
 
-const (
-	waterPerCupMl = 200
-	milkPerCupMl  = 50
-	coffeePerCupG = 15
-)
-
-func minN(nums ...int) int {
-	// нахождение минимального числа
-	if len(nums) == 0 {
-		return 0
-	}
-	min := nums[0]
-	for _, v := range nums[1:] {
-		if min > v {
-			min = v
-		}
-	}
-	return min
+type Recipe struct {
+	water, milk, beans, price, cups int
 }
 
-func howManyCups(w, m, c int) int {
-	// функция считает количество чашек которое можно приготовить из заданного кол-ва ингредиентов
-	water := w / waterPerCupMl
-	milk := m / milkPerCupMl
-	coffee := c / coffeePerCupG
-	return minN(water, milk, coffee)
+var espresso = Recipe{water: 250, milk: 0, beans: 16, cups: 1, price: 4}
+var latte = Recipe{water: 350, milk: 75, beans: 20, cups: 1, price: 7}
+var capp = Recipe{water: 200, milk: 100, beans: 12, cups: 1, price: 6}
+
+type MachineState struct {
+	water, milk, beans, cups, money int
+}
+
+func buy(state *MachineState, recipe Recipe) {
+	state.water -= recipe.water
+	state.milk -= recipe.milk
+	state.beans -= recipe.beans
+	state.cups -= recipe.cups
+	state.money += recipe.price
+}
+
+func printState(m MachineState) {
+	f.Printf("The coffee machine has: \n%d of water \n%d of milk\n%d of coffee beans\n%d disposable cups\n$%d of money\n", m.water, m.milk, m.beans, m.cups, m.money)
 }
 
 func main() {
-	var waterMl, milkMl, coffeeG int
-	f.Println("Write how many ml of water the coffee machine has:")
-	f.Scan(&waterMl)
-	f.Println("Write how many ml of milk the coffee machine has:")
-	f.Scan(&milkMl)
-	f.Println("Write how many grams of coffee the coffee machine has:")
-	f.Scan(&coffeeG)
-	f.Println("Write how many cups of coffee you will need:")
-	var cupsNeeded int
-	f.Scan(&cupsNeeded)
-
-	howMany := howManyCups(waterMl, milkMl, coffeeG)
-
-	switch {
-	case howMany == cupsNeeded:
-		f.Println("Yes, I can make that amount of coffee")
-	case howMany > cupsNeeded:
-		dif := howMany - cupsNeeded
-		f.Printf("Yes, I can make that amount of coffee (and even %v more than that) \n", dif)
-	case howMany < cupsNeeded:
-		f.Printf("No, I can make only %d cups of coffee \n", howMany)
+	var state = MachineState{water: 400, milk: 540, beans: 120, cups: 9, money: 550}
+	printState(state)
+	f.Println("Write action (buy, fill, take): ")
+	var input string
+	f.Scan(&input)
+	switch input {
+	case "buy":
+		f.Println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+		var drink int
+		f.Scan(&drink)
+		switch drink {
+		case 1:
+			buy(&state, espresso)
+		case 2:
+			buy(&state, latte)
+		case 3:
+			buy(&state, capp)
+		}
+	case "fill":
+	case "take":
 	}
+	printState(state)
 }
